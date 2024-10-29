1bd98249ccaf09a968f8601096caa80d12e63ace
Deepanjan Nag <subscriptions@deepanjannag.com>
2024-10-29 12:33:10 -0400

    IoC

--- CRUDExample/Program.cs
+++ CRUDExample/Program.cs
@@ -1,5 +1,13 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+
+//Add services into IOC container
+builder.Services.AddSingleton<ICountriesService, CountriesService>(sp => new CountriesService(true));
+builder.Services.AddSingleton<IPersonsService, PersonsService>(sp => new PersonsService(true));
+
 var app = builder.Build();
 
 if (builder.Environment.IsDevelopment())

57e95d590d357da813b948ca60cf6b66d74efc07
Deepanjan Nag <subscriptions@deepanjannag.com>
2024-10-29 11:33:53 -0400

    Mock data

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -11,10 +11,128 @@ public class PersonsService : IPersonsService
     private readonly ICountriesService _countriesService; //This field is needed by Person as a dependency
     private readonly List<Person> _persons;
 
-    public PersonsService()
+    public PersonsService(bool initialize = false)
     {
         _persons = new List<Person>();
-        _countriesService = new CountriesService(); //This is a dependency of Person
+        _countriesService = new CountriesService(initialize); //This is a dependency of Person
+
+        if (initialize)
+        {
+            var countries = _countriesService.GetAllCountries();
+            _persons.AddRange(new List<Person>
+            {
+                new()
+                {
+                    PersonID = Guid.Parse("13BFBE9A-A250-4F2D-B3BB-41DA247C1C11"),
+                    PersonName = "John Doe",
+                    Email = "p1@gmail.com",
+                    CountryID = countries[0].CountryID,
+                    Address = "123, 1st Street, New York",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1990, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("8F751823-882A-4DFE-8EF8-3FEAF215CE55"),
+                    PersonName = "Jill Tee",
+                    Email = "p2@gmail.com",
+                    CountryID = countries[1].CountryID,
+                    Address = "234, 1st Street, Oregon",
+                    Gender = GenderOptions.Female.ToString(),
+                    DateOfBirth = new DateTime(1990, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("4C3D8781-5687-4815-B4FE-0677DA31DDDE"),
+                    PersonName = "Peter Norton",
+                    Email = "p3@gmail.com",
+                    CountryID = countries[2].CountryID,
+                    Address = "234, 13th Street, Florida",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1960, 1, 1),
+                    ReceiveNewsLetters = false
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("15AA0CC2-40B8-49C5-8DA1-BC64C49A8B6A"),
+                    PersonName = "Lee Morgan",
+                    Email = "p4@gmail.com",
+                    CountryID = countries[3].CountryID,
+                    Address = "234, 1st Street, Connecticut",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1980, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("27381194-EAF6-4E28-A033-7C857AEFA373"),
+                    PersonName = "Lady Ada",
+                    Email = "p5@hotmail.com",
+                    CountryID = countries[4].CountryID,
+                    Address = "Alpha, 1st Street, New Jersey",
+                    Gender = GenderOptions.Female.ToString(),
+                    DateOfBirth = new DateTime(1980, 4, 4),
+                    ReceiveNewsLetters = false
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("BA353F8D-D0A3-4323-8ED0-8FDB32A2F219"),
+                    PersonName = "Ravi Kumar",
+                    Email = "p6@gmail.com",
+                    CountryID = countries[0].CountryID,
+                    Address = "321, 1st Street, New York",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1960, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("B67C966B-965B-44CD-8BD1-F7E6FBCADB26"),
+                    PersonName = "Sita Kaur",
+                    Email = "p7@gmail.com",
+                    CountryID = countries[1].CountryID,
+                    Address = "456, 1st Street, Oregon",
+                    Gender = GenderOptions.Female.ToString(),
+                    DateOfBirth = new DateTime(1950, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("8D1E7BCE-B8E4-42C0-B178-DF573190E1B0"),
+                    PersonName = "Ujwal Banerjee",
+                    Email = "p8@gmail.com",
+                    CountryID = countries[2].CountryID,
+                    Address = "654, 13th Street, Florida",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1990, 1, 1),
+                    ReceiveNewsLetters = false
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("67CC8CAC-23AB-4803-98C5-C2EEDD3F611B"),
+                    PersonName = "Santosh Singh",
+                    Email = "p9@gmail.com",
+                    CountryID = countries[3].CountryID,
+                    Address = "2379, 1st Street, Connecticut",
+                    Gender = GenderOptions.Male.ToString(),
+                    DateOfBirth = new DateTime(1975, 1, 1),
+                    ReceiveNewsLetters = true
+                },
+                new()
+                {
+                    PersonID = Guid.Parse("FCF2AF1D-1D53-475D-AB89-30E745626BCC"),
+                    PersonName = "Prashant Kuleshwar",
+                    Email = "p10@hotmail.com",
+                    CountryID = countries[4].CountryID,
+                    Address = "Beta, 1st Street, New Jersey",
+                    Gender = GenderOptions.Female.ToString(),
+                    DateOfBirth = new DateTime(1984, 4, 4),
+                    ReceiveNewsLetters = false
+                }
+            });
+        }
     }
 
     public PersonResponse AddPerson(PersonAddRequest? personAddRequest)

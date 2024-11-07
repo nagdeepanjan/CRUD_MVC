8867f36cc84efa5306635091cfbac61e7b1fccb3
Deepanjan Nag <subscriptions@deepanjannag.com>
2024-11-07 18:05:20 -0500

    CountriesRepo

--- RepositoryContracts/ICountriesRepository.cs
+++ RepositoryContracts/ICountriesRepository.cs
@@ -1,5 +1,13 @@
-﻿namespace RepositoryContracts;
+﻿using Entities;
+
+namespace RepositoryContracts;
 
 public interface ICountriesRepository
 {
+    Task<Country> AddCountry(Country country);
+    Task<List<Country>> GetAllCountries();
+
+    Task<Country?> GetCountryByCountryID(Guid countryID);
+
+    Task<Country?> GetCountryByCountryName(string countryName);
 }

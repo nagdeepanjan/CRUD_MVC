17331b1d893cd2b1b5edf64a0152df3c21d4e079
Deepanjan Nag <subscriptions@deepanjannag.com>
2024-11-08 11:42:07 -0500

    Repos

--- RepositoryContracts/IPersonsRepository.cs
+++ RepositoryContracts/IPersonsRepository.cs
@@ -25,7 +25,7 @@ public interface IPersonsRepository
     /// </summary>
     /// <param name="personID">PersonID (guid) to search</param>
     /// <returns>A person object or null</returns>
-    Task<List<Person>> GetPersonByPersonID(Guid personID);
+    Task<Person?> GetPersonByPersonID(Guid personID);
 
 
     /// <summary>
